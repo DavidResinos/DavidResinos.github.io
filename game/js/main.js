333ae15788a079cc8e31d9a8138601a26e525ea6
David <resinosdavid@gmail.com>
2017-07-17 17:50:10 -0400

    audio control

--- game/js/main.js
+++ game/js/main.js
@@ -331,7 +331,8 @@ PlayState._createHud = function () {
     this.hud.add(this.keyIcon);
     this.hud.position.set(10, 10);
 };
-myAudio = new Audio('audio/bgm.mp3'); 
+myAudio = new Audio('audio/bgm.mp3');
+audio.volume = 0.2;
 myAudio.addEventListener('ended', function() {
     this.currentTime = 0;
     this.play();
