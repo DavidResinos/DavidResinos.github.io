a8962c1ac4678897e2ce1e2f62638035db2c1b46
David <resinosdavid@gmail.com>
2017-07-18 17:52:30 -0400

    new lvl

--- game/js/main.js
+++ game/js/main.js
@@ -95,7 +95,7 @@ Spider.prototype.die = function () {
 };
 PlayState = {};
 
-const LEVEL_COUNT = 2;
+const LEVEL_COUNT = 3;
 
 PlayState.init = function (data) {
     this.game.renderer.renderSession.roundPixels = true;
@@ -121,7 +121,7 @@ PlayState.init = function (data) {
 PlayState.preload = function () {
     this.game.load.json('level:0', 'data/level00.json');
     this.game.load.json('level:1', 'data/level01.json');
-
+    this.game.load.json('level:2', 'data/level02.json');
     this.game.load.image('font:numbers', 'images/numbers.png');
 
     this.game.load.image('background', 'images/background.png');
